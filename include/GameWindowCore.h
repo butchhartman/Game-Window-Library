ba0b819af6d8263361ff55d54e67660adac47245
butchhartman <rcamacho5436@gmail.com>
2025-06-04 12:14:13 -0700

    Added documentation to the core API header

--- include/GameWindowCore.h
+++ include/GameWindowCore.h
@@ -2,50 +2,69 @@
 #define _GAMEWINDOW_CORE_H_
 #include <stdint.h>
 #include <stdalign.h>
-// Things I'll need
-// Struct to store window state and info [X]
-// Function to create a window [X]
-// A way to run the window's message loop without halting the main thread [X]
-// A way to queue messages so their callback functions can be called on the main thread (poll events) [X]
-// Function to show a window [X]
-// Function to hide a window [X]
-// Callback function for keyboard input processing [X]
-// Function to lock mouse to window [X]
-// Callback function for mouse input processing [X] 
-// Function to create an opengl context on a window [X] 
-// Function to make a window's opengl context current  -- is done automatically, which means that having multiple windows with different contexts is unsupported
-// Function to swap buffers [X]
-// Function to clean up & destroy a window [X]
 
+/**
+ *  Identifier for the type of event recieved by the input callback function
+ */
 typedef enum gwEventType {gw_windowReizeEvent, gw_keyboardEvent, gw_mouseEvent} gwEventType;
+/**
+ * Identifier for the key pressed when a keyboard input is recieved
+ */
 typedef enum gwEventKeycode  {gw_W, gw_A, gw_S, gw_D, gw_KEYUNSUPPORED} gwEventKeycode;
+/**
+ * Identifier for the type of input when a mouse input is recieved
+ */
 typedef enum gwMouseEventInputCode {gw_NONE, gw_MOVE, gw_LMB, gw_RMB} gwMouseEventInputCode;
 
-// bit 1 is up/down
+/**
+ * 64 bit unsigned integer representing a bitfield of describing the state
+ * of the recieved keyboard input
+ */
 typedef uint64_t keyStateFlags;
+/**
+ * 64 bit unsigned integer representing a bitfield describing the state of the recieved mouse input
+ */
 typedef uint64_t mouseInputStateFlags;
 
+/**
+ * Simple enum meant to reduce magic numbers in code. Describes whether or not a
+ * window is active. Activity is defined by a window being visible and
+ * processing events.
+ */
 typedef enum gwWindowStatus {
     GW_WINDOW_INACTIVE,
     GW_WINDOW_ACTIVE
 } gwWindowStatus;
 
+/**
+ * Flags which describe window-wide behavior regarding inputs
+ */
 typedef enum inputFlagBits {
     CAPTURE_MOUSE_BIT = 0x0000000000000001, // bit 1
 } inputFlagBits;
 
+/**
+ * Flags which describe information about a key input
+ */
 typedef enum KeyStateFlagBits{
     KEY_UP_BIT = 0x0000000000000000, // bit 1
     KEY_DOWN_BIT = 0x0000000000000001, // bit 1
 } KeyStateFlagBits;
 
+/**
+ * Flags which describe information about a mouse input
+ */
 typedef enum mouseInputStateFlagBits {
     MOUSE_BUTTON_UP_BIT = 0x0000000000000000, // bit 1
     MOUSE_BUTTON_DOWN_BIT = 0x0000000000000001, // bit 1
 } mouseInputStateFlagBits;
 
-// TODO: Consider shortening this struct by making all fields generic and their expected values dependant on the event type
+/**
+ * Struct that the input callback will recieve and need to parse in order to
+ * determine what kind of input was recieved and the state of that input.
+ */
 typedef struct gwInputEvent {
+// TODO: Consider shortening this struct by making all fields generic and their expected values dependant on the event type
     gwEventType eventType;
 
     // window resize
@@ -57,46 +76,104 @@ typedef struct gwInputEvent {
     keyStateFlags keyStateFlags;
     
     // mouse
-    // lmb/rmb, up/down,
     gwMouseEventInputCode mouseInputCode;
+    uint32_t padding1;
     mouseInputStateFlags mouseInputStateFlags;
     int64_t xPos;
     int64_t yPos;
 } gwInputEvent;
 
+/**
+ * Opaque struct object which represents the window which the user will interact with
+ */
 typedef struct GameWindow GameWindow;
 
+/**
+ * Function pointer type describing the expected signature of the input callback function
+ */
 typedef void (*PTRINPUTCBFUNC)(GameWindow* window, gwInputEvent inputEvent);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
+/**
+ * Prints a string stating the current library version that is in use, followed by a new line
+ */
 extern void gwlPrintVersion(void);
 
+/**
+ * Creates and returns a pointer to a GameWindow object
+ * \param windowTitle The title that will be displayed on the window frame
+ */
 extern GameWindow* gwlCreateWindow(const char* windowTitle);
 
+/**
+ * Makes visible and enabled input processing for the passed window
+ * \param window The window to be displayed
+ */
 extern void gwlShowWindow(GameWindow* window);
 
+/**
+ * Hides and disables input processing for the passed window 
+ * \note The window does not process or queue any new inputs in this state, it is inert.
+ * \param window The window to be hidden
+ */
 extern void gwlHideWindow(GameWindow* window);
 
+/**
+ * Destroys the passed window and frees all of its resources
+ * \param window The window to be destroyed
+ */
 extern void gwlCleanupWindow(GameWindow* window);
 
+/**
+ * Returns if the window is active or not
+ * \param window - The window whose status will be queried
+ * \return GW_WINDOW_ACTIVE if active, GW_WINDOW_INACTIVE if inactive
+ */
 extern uint64_t gwlGetWindowStatus(GameWindow* window);
 
+/**
+ * Flushes the passed window's event queue, processing all its events
+ * \param window The window whose events will be processed
+ */
 extern void gwlPollEvents(GameWindow* window);
 
+/**
+ * Sets the passed callback as the function the window will call whenever an
+ * input event is processed
+ * \param window The window whose callback will be set
+ * \param callback The input callback function to set
+ */
 extern void gwlSetInputCallback(GameWindow* window, PTRINPUTCBFUNC callback);
 
+/**
+ * Changes the window's global input processing behavior based on the passed flags
+ * \param window The window whose flags will be modified
+ * \param flags The flags to modify
+ * \param state The state the flags will take (1 for active, 0 for inactive)
+ * \note All of a window's flags are set to inactive by default. This function
+ * does not modifiy any other flags besides the ones passed to the flags
+ * parameter
+ */
 extern void gwlSetInputFlags(GameWindow* window, inputFlagBits flags, int8_t state);
 
+/**
+ * Creates an OpenGL context for the passed window
+ * \param window A pointer to the gamewindow that will be used to create the context
+ * \note This function MAY be forced to re-create the passed window, hence the double pointer. 
+ */
 extern void gwlCreateOpenGLContext(GameWindow** window);
 
+/**
+ * Swaps the framebuffers of the passed window
+ * \param window The window whose framebuffers will be swapped
+ */
 extern void gwlSwapBuffers(GameWindow* window);
 
 #ifdef __cplusplus
-}
+};
 #endif
 
 #endif // _GAMEWINDOW_PROTOTYPES_H
